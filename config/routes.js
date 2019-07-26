938b8e41d724ba8d6bd4ab46ab4bc551087ce7cf
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-26 08:31:10 +0100

    Add register endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const UsersDB = require('../users');
 const { authenticate, generateToken, generateHash, verifyPassword } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -8,8 +8,18 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  try {
+    const {username, password} = req.body;
+    if (!username || !password) {
+      res.status(400).json({ error: "Username and password needs to be provided" });
+    }
+    const hash = generateHash(password);
+    await UsersDB.insert({ username, password: hash });
+    res.status(200).json({ username });
+  } catch (error) {
+    res.status(500).json({ error: "Couldn't register user" });
+  }
 }
 
 function login(req, res) {
