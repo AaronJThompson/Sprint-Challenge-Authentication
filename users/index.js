c0bb3c6979e64fb21c589f74e07f137b4193f1e9
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-26 08:23:47 +0100

    Add data manipulation functions for db

--- users/index.js
+++ users/index.js
@@ -11,3 +11,16 @@ function findById(id) {
         .where({ id })
         .first();
 }
+
+function insert(user) {
+    return db('users')
+        .insert(user)
+        .then(ids => findById(ids[0]));
+}
+
+function update(id, fields) {
+    return db('users')
+        .where({id})
+        .update(fields)
+        .then(() => findById(id));
+}
